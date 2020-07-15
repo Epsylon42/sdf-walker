a2b033d27dacfa85ac75db32b55104e34a3ff728
Epsylon <eepsylon.3@gmail.com>
2020-07-15 20:33:47 +0300

    Add ambient occlusion

--- src/glsl/footer.glsl
+++ src/glsl/footer.glsl
@@ -12,18 +12,39 @@ vec3 normal(vec3 p, float d) {
 
 const int steps = 50;
 const float max_dist = 2000;
-const vec3 ambient = vec3(0.24, 0.09, 0.4);
+const vec3 sky = vec3(0.24, 0.09, 0.4);
+const vec3 ambient = sky * 0.9;
 const float delta = 0.01;
 const float shadow_coef = 64;
+
+float ambient_occlusion(vec3 pos, vec3 nrm) {
+    const float max_dist = 10;
+    const int steps = 6;
+
+    float factor = 0;
+    for (int i = 1; i < steps + 1; i++) {
+        float frac = float(i) / float(steps);
+        frac *= frac;
+
+        float dist = max_dist * frac;
+        dist = dist + pow(sin((pos.x + pos.y + pos.z) / 3 + dist), 2.0);
+        vec3 p = pos + nrm * dist;
+
+        factor += (map(p).w) / dist / steps;
+    }
+
+    return (factor + 0.4) / 1.4;
+}
 
 vec3 color_at(vec3 pos, vec3 background, float opacity, Shadow shadow) {
     vec3 nlight = -normalize(light);
     float dist = distance(pos, cam_pos);
     vec3 nrm = normal(pos, delta);
 
-    vec3 diffuse = apply_mask(map(pos).xyz, shadow.mask) * shadow.shadow;
+    vec3 diffuse = map(pos).xyz;
+    vec3 reflected = apply_mask(diffuse, shadow.mask) * shadow.shadow;
 
-    return mix(background, max(ambient/2, diffuse), opacity);
+    return mix(background, max(ambient * diffuse * ambient_occlusion(pos, nrm), reflected), opacity);
 }
 
 
@@ -58,7 +79,7 @@ Shadow calc_shadow(vec3 pos) {
 
 vec3 march(vec3 pos, vec3 dir) {
     vec3 nlight = -normalize(light);
-    vec3 background = vec3(pow(max(0, dot(dir, nlight)), 10) * 5) + ambient;
+    vec3 background = vec3(pow(max(0, dot(dir, nlight)), 10) * 5) + sky;
 
     int i = 0;
     vec3 mask = vec3(1);
