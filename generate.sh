e0fb8c0209fd3b6b1cbfa5802a4274063d940769
Epsylon <eepsylon.3@gmail.com>
2020-11-28 18:21:48 +0200

    Add an argument parser

--- generate.sh
+++ generate.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-set -e
+set -eu
 
-cargo run --release -- $1 | ffmpeg -f rawvideo -pix_fmt rgb24 -r 24 -s:v 800x600 -i - -c:v h264 -crf 0 -pix_fmt yuv420p out.mp4
+width=$2
+height=$3
+
+if [ -z "$4" ]; then
+    fps=30
+else
+    fps=$4
+fi
+
+cargo run --release -- $1 render --width $width --height $height --fps $fps | ffmpeg -f rawvideo -pix_fmt rgb24 -r $fps -s:v "$width"x"$height" -i - -c:v h264 -crf 0 -pix_fmt yuv420p out.mp4
--- src/main.rs
+++ src/main.rs
@@ -4,56 +4,74 @@
 extern crate nalgebra_glm as glm;
 #[macro_use] extern crate lazy_static;
 
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
 mod shaders;
 mod rendering;
 
 use shaders::*;
 use rendering::{onscreen::new_app, offscreen::new_app_offscreen};
 
+#[derive(StructOpt)]
+struct Opt { 
+    source: PathBuf,
+
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(StructOpt)]
+enum Command {
+    Render {
+        #[structopt(long)]
+        width: u32,
+        #[structopt(long)]
+        height: u32,
+        #[structopt(long, default_value = "30")]
+        fps: f32,
+    },
+
+    Interactive {
+        #[structopt(long, short)]
+        camera: bool,
+    }
+}
+
 fn main() {
-    let source_path = std::env::args()
-        .nth(1)
-        .unwrap_or_else(|| String::from("test.scene"));
+    let opt = Opt::from_args();
 
-    let mut loader = SceneDescLoader::new(source_path);
+    let mut loader = SceneDescLoader::new(opt.source);
 
-    for arg in std::env::args().skip(2).chain(std::iter::once("".into())) {
-        match arg.as_str() {
-            "nocamera" => {
-                loader.switch_camera(false);
-            }
-
-            "interactive" => {
-                let (app, el) = new_app([800, 600], loader);
-                app.run(el);
-            }
-
-            _ => {
-                let scene = loader.load().unwrap();
-
-                let duration = scene
-                    .camera
-                    .as_ref()
-                    .map(|cam| cam.duration().ceil())
-                    .unwrap_or(10.0) as usize;
-
-                let (mut app, _) = new_app_offscreen([800, 600], scene);
-
-                let fps = 24;
-
-                let stdout = std::io::stdout();
-                let mut stdout = stdout.lock();
-
-                for i in 0..(fps * duration) {
-                    use std::io::Write;
-
-                    app.draw(i as f32 / fps as f32);
-                    stdout.write_all(&app.to_image().into_raw()).unwrap();
-                }
-                return;
-            }
+    match opt.command {
+        Command::Render { width, height, fps } => render(loader, [width, height], fps),
+        Command::Interactive { camera } => {
+            loader.switch_camera(camera);
+            let (app, el) = new_app([800, 600], loader);
+            app.run(el);
         }
     }
 }
 
+fn render(mut loader: SceneDescLoader, size: [u32; 2], fps: f32) {
+    let scene = loader.load().unwrap();
 
+    let duration = scene
+        .camera
+        .as_ref()
+        .map(|cam| cam.duration().ceil())
+        .unwrap_or(10.0) as usize;
+
+    let (mut app, _) = new_app_offscreen(size, scene);
+
+    let stdout = std::io::stdout();
+    let mut stdout = stdout.lock();
+
+    for i in 0..(fps * duration as f32) as u32 {
+        use std::io::Write;
+
+        app.draw(i as f32 / fps);
+        stdout.write_all(&app.to_image().into_raw()).unwrap();
+    }
+}
