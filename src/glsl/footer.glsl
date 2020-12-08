585c14f2b178ea0da4d38fd6684822edd83f8002
Epsylon <eepsylon.3@gmail.com>
2020-12-08 23:04:14 +0200

    Make a cool scene

--- src/glsl/footer.glsl
+++ src/glsl/footer.glsl
@@ -11,7 +11,15 @@ vec3 normal(vec3 p, float d) {
     float dy = map(p + vec3(0, d, 0)).w - map(p - vec3(0, d, 0)).w;
     float dz = map(p + vec3(0, 0, d)).w - map(p - vec3(0, 0, d)).w;
 
-    return normalize(vec3(dx, dy, dz));
+    vec3 grad = vec3(dx, dy, dz);
+    if (length(grad) == 0)
+    {
+        vec3(0);
+    }
+    else
+    {
+        return normalize(grad);
+    }
 }
 
 const int steps = 50;
@@ -68,7 +76,8 @@ Shadow calc_shadow(vec3 pos) {
     vec3 nlight = -normalize(light);
     vec3 nrm = normal(pos, delta);
 
-    pos += nrm * delta * 4;
+    pos += normalize(screen_pos - pos) * delta * 4;
+    //pos += nrm * delta * 4;
 
     float closest = 1.0;
 
@@ -100,30 +109,42 @@ vec3 march(vec3 pos, vec3 dir) {
     vec3 nlight = -normalize(light);
     vec3 background = vec3(pow(max(0, dot(dir, nlight)), 10) * 5) + sky;
 
-    int i = 0;
-    vec3 mask = vec3(1);
-    for (float t = 0; t < max_dist;) {
-        vec4 m = map(pos + dir*t);
-        float od = m.w;
+    for (int tries = 0; tries < 3; tries += 1) {
+        int i = 0;
+        vec3 mask = vec3(1);
+        bool continue_outer = false;
+        for (float t = 0; t < max_dist;) {
+            vec4 m = map(pos + dir*t);
+            float od = m.w;
 
-        MapTransparent transparent = map_transparent(pos + dir*t);
+            MapTransparent transparent = map_transparent(pos + dir*t);
 
-        if (transparent.d < delta) {
-            mask -= transparent.color.xyz * transparent.color.w * abs(transparent.d);
-            t += max(abs(transparent.d), delta);
-        } else if (od < delta) {
-            float itershade = smoothstep(35, 5, i) * 0.5 + 0.5;
-            float distshade = smoothstep(max_dist, 0, t);
-            vec3 solid_color = color_at(pos + dir*t, background, distshade * itershade, calc_shadow(pos + dir*t));
-            return apply_mask(solid_color, mask);
-        } else {
-            t += max(min(od, transparent.d), delta);
+            if (transparent.d < delta) {
+                mask -= transparent.color.xyz * transparent.color.w * abs(transparent.d);
+                t += max(abs(transparent.d), delta);
+            } else if (od < delta) {
+                if (normal(pos + dir*t, delta) == vec3(0) && tries < 3) {
+                    continue_outer = true;
+                    break;
+                }
+
+                float itershade = smoothstep(35, 5, i) * 0.5 + 0.5;
+                float distshade = smoothstep(max_dist, 0, t);
+                vec3 solid_color = color_at(pos + dir*t, background, distshade * itershade, calc_shadow(pos + dir*t));
+                return apply_mask(solid_color, mask);
+            } else {
+                t += max(min(od, transparent.d), delta) * (1 - 0.1 * tries);
+            }
+
+            i += 1;
         }
 
-        i += 1;
-    }
+        if (continue_outer) {
+            continue;
+        }
 
-    return apply_mask(background, mask);
+        return apply_mask(background, mask);
+    }
 }
 
 void main() {
